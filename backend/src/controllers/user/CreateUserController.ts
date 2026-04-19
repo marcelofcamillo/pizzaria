62fd0bfe6549c52a9876b224f9a7ec027292bb45
Marcelo Camillo <marcelocamillo18@gmail.com>
2026-04-19 01:48:51 -0300

    Cadastrando usuário

--- backend/src/controllers/user/CreateUserController.ts
+++ backend/src/controllers/user/CreateUserController.ts
@@ -4,10 +4,13 @@ import { CreateUserService } from '../../services/CreateUserService';
 class CreateUserController {
   async handle(req: Request, res: Response) {
     const { name, email, password } = req.body;
-    console.log({ name, email, password });
 
     const createUserService = new CreateUserService();
-    const user = await createUserService.execute();
+    const user = await createUserService.execute({
+      name: name,
+      email: email,
+      password: password,
+    });
 
     res.json({ message: user });
   }
--- backend/src/services/CreateUserService.ts
+++ backend/src/services/CreateUserService.ts
@@ -1,8 +1,32 @@
-class CreateUserService {
-  async execute() {
-    console.log('Executando serviço.');
+import prismaClient from '../prisma';
 
-    return 'Usuário criado.';
+interface CreateUserProps {
+  name: String;
+  email: String;
+  password: String;
+}
+
+class CreateUserService {
+  async execute({ name, email, password }: CreateUserProps) {
+    const userAlreadyExists = await prismaClient.user.findFirst({
+      where: {
+        email: email,
+      },
+    });
+
+    if (userAlreadyExists) {
+      throw new Error('Usuário já existente.');
+    }
+
+    const user = await prismaClient.user.create({
+      data: {
+        name: name,
+        email: email,
+        password: password,
+      },
+    });
+
+    return user.name;
   }
 }
 
