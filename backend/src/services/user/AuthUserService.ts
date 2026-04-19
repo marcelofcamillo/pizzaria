f862089f3b819fcfa288058a2955c62f846f6bd0
Marcelo Camillo <marcelocamillo18@gmail.com>
2026-04-19 02:32:12 -0300

    Realizando o login

--- backend/src/services/user/AuthUserService.ts
+++ backend/src/services/user/AuthUserService.ts
@@ -1,3 +1,7 @@
+import { compare } from 'bcryptjs';
+import prismaClient from '../../prisma';
+import { sign } from 'jsonwebtoken';
+
 interface AuthUserServiceProps {
   email: string;
   password: string;
@@ -5,9 +9,42 @@ interface AuthUserServiceProps {
 
 class AuthUserService {
   async execute({ email, password }: AuthUserServiceProps) {
-    console.log({ email, password });
+    const user = await prismaClient.user.findFirst({
+      where: {
+        email: email,
+      },
+    });
 
-    return 'Logado.';
+    if (!user) {
+      throw new Error('E-mail/senha é obrigatório.');
+    }
+
+    const passwordMatch = await compare(password, user.password);
+
+    if (!passwordMatch) {
+      throw new Error('E-mail/senha é obrigatório.');
+    }
+
+    // Gerar o TOKEN JWT
+    const token = sign(
+      {
+        name: user.name,
+        email: user.email,
+      },
+      process.env.JWT_SECRET as string,
+      {
+        subject: user.id,
+        expiresIn: '30d',
+      },
+    );
+
+    return {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      role: user.role,
+      token: token,
+    };
   }
 }
 
